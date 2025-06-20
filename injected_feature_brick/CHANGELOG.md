f1d643cce5639315bc3a6befa9a8635a5183110b
Shihab Kandil <shihab.mostafa1@gmail.com>
2025-06-20 23:56:03 +0300

    Add post generation hook to run build_runner after mason generation

--- injected_feature_brick/CHANGELOG.md
+++ injected_feature_brick/CHANGELOG.md
@@ -2,3 +2,4 @@
 
 - Add feature structure
 - Add creation of abstract repository and implementation repository injected into {{feature}}_cubit
+- Add post mason generation hook to run `build_runner` flutter command
--- injected_feature_brick/hooks/post_gen.dart
+++ injected_feature_brick/hooks/post_gen.dart
@@ -1,5 +1,28 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+void run(HookContext context) async {
+  final progress = context.logger
+      .progress('Generating code using `build_runner`, this may take a while...');
+
+  try {
+    final result = await Process.run(
+      'flutter',
+      ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'],
+    );
+
+    if (result.exitCode == 0) {
+      context.logger.success('$green Build completed successfully!');
+      progress.complete();
+    } else {
+      context.logger.err('\nBuild failed with exit code ${result.exitCode}');
+      progress.fail();
+    }
+  } catch (e) {
+    progress.fail();
+    context.logger.err('Error running build_runner $e');
+  }
+
+  
 }
